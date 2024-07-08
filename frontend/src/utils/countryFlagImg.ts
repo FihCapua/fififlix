10ff50216b6778053d4cfd00a2c801334b35daa2
FihCapua <fihcapua@gmail.com>
2024-07-08 17:57:24 -0300

    feat: update movie origins flags

--- frontend/src/utils/countryFlagImg.ts
+++ frontend/src/utils/countryFlagImg.ts
@@ -1,9 +1,21 @@
 import eua from "../assets/images/usa.svg";
+import japan from "../assets/images/japan.svg";
+import czechRepublic from "../assets/images/czech-republic.svg";
+import uruguay from "../assets/images/uruguay.svg";
 
 export const countryFlag = (country: string) => {
   switch (country) {
   case "Estados Unidos":
     return eua;
+
+  case "Japão":
+    return japan;
+
+  case "República Checa":
+    return czechRepublic;
+
+  case "Uruguai":
+    return uruguay;
 
   default:
     return null;
